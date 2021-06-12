d619c46b314c68e3353e26268593bd3e15426a28
Antony Vasilev <antony.n.vasilev@gmail.com>
2021-06-12 22:46:41 +0300

    added rest api

--- app/run_server.py
+++ app/run_server.py
@@ -29,10 +29,10 @@ def load_model(model_path):
 
 @app.route("/", methods=["GET"])
 def general():
-    return "Welcome to fraudelent prediction process"
+    return "Welcome to airline passenger satisfaction prediction process"
 
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
     # initialize the data dictionary that will be returned from the
     # view
@@ -41,71 +41,82 @@ def predict():
     # ensure an image was properly uploaded to our endpoint
     if flask.request.method == "POST":
 
-        id_, gender, customer_type, age, type_of_travel = '', '', '', '', ''
-        class_, flight_distance, wifi_service = '', '', ''
-        time_convenient, ease_of_online_booking = '', ''
-        gate_location, food_and_drink, online_boarding = '', '', ''
-        seat_comfort, inflight_entertainment, on_board_service = '', '', ''
-        leg_room_service, baggage_handling, checkin_service = '', '', ''
-        inflight_service, cleanliness, departure_delay, arrival_delay = '', '', '', ''
+        # gender, customer_type, age, type_of_travel = '', '', '', ''
+        # class_, flight_distance, wifi_service = '', '', ''
+        # ease_of_online_booking, inflight_service = '', ''
+        # food_and_drink, online_boarding, cleanliness = '', '', ''
+        # seat_comfort, inflight_entertainment, on_board_service = '', '', ''
+        # leg_room_service, baggage_handling, checkin_service = '', '', ''
 
         request_json = flask.request.get_json()
+        # print(1)
 
-        if request_json["id"]:
-            id_ = request_json['id']
-        if request_json["Gender"]:
-            gender = request_json['Gender']
-        if request_json["Customer Type"]:
-            customer_type = request_json['Customer Type']
-        if request_json["Age"]:
-            age = request_json['Age']
-        if request_json["Type of Travel"]:
-            type_of_travel = request_json['Type of Travel']
-        if request_json["Class"]:
-            class_ = request_json['Class']
-        if request_json["Flight Distance"]:
-            flight_distance = request_json['Flight Distance']
-        if request_json["Inflight wifi service"]:
-            wifi_service = request_json['Inflight wifi service']
-        if request_json["Departure/Arrival time convenient"]:
-            time_convenient = request_json['Departure/Arrival time convenient']
-        if request_json["Ease of Online booking"]:
-            ease_of_online_booking = request_json['Ease of Online booking']
-        if request_json["Gate location"]:
-            gate_location = request_json['Gate location']
-        if request_json["Food and drink"]:
-            food_and_drink = request_json['Food and drink']
-        if request_json["Online boarding"]:
-            online_boarding = request_json['Online boarding']
-        if request_json["Seat comfort"]:
-            seat_comfort = request_json['Seat comfort']
-        if request_json["Inflight entertainment"]:
-            inflight_entertainment = request_json['Inflight entertainment']
-        if request_json["On-board service"]:
-            on_board_service = request_json['On-board service']
-        if request_json["Leg room service"]:
-            leg_room_service = request_json['Leg room service']
-        if request_json["Baggage handling"]:
-            baggage_handling = request_json['Baggage handling']
-        if request_json["Checkin service"]:
-            checkin_service = request_json['Checkin service']
-        if request_json["Inflight service"]:
-            inflight_service = request_json['Inflight service']
-        if request_json["Cleanliness"]:
-            cleanliness = request_json['Cleanliness']
-        if request_json["Departure Delay in Minutes"]:
-            departure_delay = request_json['Departure Delay in Minutes']
-        if request_json["Arrival Delay in Minutes"]:
-            arrival_delay = request_json['Arrival Delay in Minutes']
+        # if request_json["Gender"]:
+        #     gender = request_json['Gender']
+        # if request_json["Customer Type"]:
+        #     customer_type = request_json['Customer Type']
+        # if request_json["Age"]:
+        #     age = request_json['Age']
+        # if request_json["Type of Travel"]:
+        #     type_of_travel = request_json['Type of Travel']
+        # if request_json["Class"]:
+        #     class_ = request_json['Class']
+        # if request_json["Flight Distance"]:
+        #     flight_distance = request_json['Flight Distance']
+        # if request_json["Inflight wifi service"]:
+        #     wifi_service = request_json['Inflight wifi service']
+        # if request_json["Ease of Online booking"]:
+        #     ease_of_online_booking = request_json['Ease of Online booking']
+        # if request_json["Food and drink"]:
+        #     food_and_drink = request_json['Food and drink']
+        # if request_json["Online boarding"]:
+        #     online_boarding = request_json['Online boarding']
+        # if request_json["Seat comfort"]:
+        #     seat_comfort = request_json['Seat comfort']
+        # if request_json["Inflight entertainment"]:
+        #     inflight_entertainment = request_json['Inflight entertainment']
+        # if request_json["On-board service"]:
+        #     on_board_service = request_json['On-board service']
+        # if request_json["Leg room service"]:
+        #     leg_room_service = request_json['Leg room service']
+        # if request_json["Baggage handling"]:
+        #     baggage_handling = request_json['Baggage handling']
+        # if request_json["Checkin service"]:
+        #     checkin_service = request_json['Checkin service']
+        # if request_json["Inflight service"]:
+        #     inflight_service = request_json['Inflight service']
+        # if request_json["Cleanliness"]:
+        #     cleanliness = request_json['Cleanliness']
+        # print(1)
 
-        preds = model.predict_proba(pd.DataFrame({"description": [description],
-                                                  "company_profile": [company_profile],
-                                                  "benefits": [benefits]}))
-        data["predictions"] = preds[:, 1][0]
-        data["description"] = description
+        # df = pd.DataFrame({"Gender": [gender],
+        #                    "Customer Type": [customer_type],
+        #                    "Age": [age],
+        #                    "Type of Travel": [type_of_travel],
+        #                    "Class": [class_],
+        #                    "Flight Distance": [flight_distance],
+        #                    "Inflight wifi service": [wifi_service],
+        #                    "Ease of Online booking": [ease_of_online_booking],
+        #                    "Food and drink": [food_and_drink],
+        #                    "Online boarding": [online_boarding],
+        #                    "Seat comfort": [seat_comfort],
+        #                    "Inflight entertainment": [inflight_entertainment],
+        #                    "On-board service": [on_board_service],
+        #                    "Leg room service": [leg_room_service],
+        #                    "Baggage handling": [baggage_handling],
+        #                    "Checkin service": [checkin_service],
+        #                    "Inflight service": [inflight_service],
+        #                    "Cleanliness": [cleanliness]})
+
+        df = pd.DataFrame(request_json)
+        # print(1)
+        preds = model.predict(df)
+        # preds = model.predict(df)
+
+        data["predictions"] = preds[0]
         # indicate that the request was a success
         data["success"] = True
-
+        # print(1)
     # return the data dictionary as a JSON response
     return flask.jsonify(data)
 
